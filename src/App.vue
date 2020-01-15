a5d2bc179177d71eb2edd66fdf2a64cf2632458a
Simon <348400564@qq.com>
2020-01-15 10:02:29 +0800

    喵喵电影 项目提交

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,15 @@
+<!--
+ * @Author: Simon
+ * @Date: 2020-01-14 11:16:50
+ * @LastEditTime : 2020-01-14 15:45:34
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /miaomiao/src/App.vue
+ -->
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/router/mine/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2020-01-14 16:20:37
+ * @LastEditTime: 2020-01-14 17:44:14
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /miaomiao/src/router/mine/index.js
+ */
+export default {
+    path:'/mine',
+    component: () => import('@/views/Mine')
+}
